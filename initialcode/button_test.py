076185866686d549a84313ba3b7ee32f62bb3676
muti1967 <mutishuman@gmail.com>
2025-01-19 20:14:13 -0800

    Update button_test.py

--- initialcode/button_test.py
+++ initialcode/button_test.py
@@ -1,26 +1,21 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.cleanup()
-# GPIO settings
-BUTTON_GPIO = 17  # BUTTON is connected to GPIO17 (BCM)
+
+BUTTON_GPIO = 17
 
 def button_callback(channel):
-    print("Button was pressed!")
+    print("Button Press Detected!")
 
-# Set up GPIO
+GPIO.cleanup()  # Clear any previous setup
 GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
-GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set GPIO17 as input with pull-up resistor
-
-# Add an event listener for the button
-GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_callback, bouncetime=300)
-
-print("Waiting for button press... Press CTRL+C to exit.")
+GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configure pin as input with pull-up resistor
 
 try:
+    GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_callback, bouncetime=300)
+    print("Waiting for button press...")
     while True:
-        # Keep the program running
-        time.sleep(1)
+        time.sleep(1)  # Keep the script running
 except KeyboardInterrupt:
-    print("Exiting program...")
+    print("Exiting...")
 finally:
-    GPIO.cleanup()  # Clean up GPIO on exit
+    GPIO.cleanup()
