781b6c6e3db6bb52dbdf280717376f133ac5eefc
Muti Shuman <mutishuman@gmail.com>
2025-04-11 21:01:33 -0700

    Update sensor.py

--- main/sensor.py
+++ main/sensor.py
@@ -7,6 +7,8 @@ import time
 # Add parent directory to Python path to find initialcode module
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from initialcode.PAJ7620U2 import PAJ7620U2
+from config import (PAJ_INT_FLAG1, PAJ_UP, PAJ_DOWN, 
+                   PAJ_LEFT, PAJ_RIGHT, PAJ_FORWARD, PAJ_BACKWARD)
 
 # Wrap the sensor class for uniformity
 class GestureSensor(PAJ7620U2):
